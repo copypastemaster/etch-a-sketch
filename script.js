a2f942b7d8f928bddfc0d0bdf66a80fe7e6da932
Koki <enoch.binas@gmail.com>
2023-02-04 01:33:21 +0800

    Button added to remove or erase styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const div = document.createElement('div');
-div.style.cssText = "border: 1px solid black; display: flex; flex-wrap: wrap; max-width: 352px; max-height: 350px;"
+div.style.cssText = "border: 1px solid black; display: flex; flex-wrap: wrap; max-width: 352px; max-height: 350px; margin: auto;"
 document.body.appendChild(div);
 
 
@@ -7,8 +7,9 @@ document.body.appendChild(div);
 function test () {
    const eraseButton = document.createElement('button');
       document.body.insertBefore(eraseButton, div);
-      eraseButton.textContent = '';
-      eraseButton.style.cssText = 'margin-bottom: 20px;';
+      eraseButton.textContent = 'Erase';
+      eraseButton.style.cssText = 'margin-bottom: 20px; margin-left: 48%';
+
 
 
    for (let i = 0; i<256; i++) {
@@ -17,8 +18,14 @@ function test () {
       anotherDiv.style.cssText = "border: 1px solid black; height: 20px; width: 20px;"
       anotherDiv.addEventListener('mouseover', () => {
          anotherDiv.style.background = 'black';
+
+         eraseButton.addEventListener('click', () => {
+            anotherDiv.style.background = 'white';
+         })
       })
    }
+
+   
 }
 
 

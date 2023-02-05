44c2aa67fafb7f0acf1ad83e868ff88fb7b7ac47
Koki <enoch.binas@gmail.com>
2023-02-05 23:33:48 +0800

    RGB functionality added

--- script.js
+++ script.js
@@ -1,26 +1,93 @@
 const mainDiv = document.querySelector('.wrapper');
-      mainDiv.style.cssText = 'border: 1px solid black;'
+
 const div = document.createElement('div');
-      div.style.cssText = 'border: 1px solid black; display: flex; flex-wrap: wrap; max-width: 700px; max-height: auto; margin-top: 50px; overflow: hidden;';
+      div.style.cssText = 'display: flex; flex-wrap: wrap; max-width: 700px; max-height: auto; margin-top: 20px; overflow: hidden; border: 2px solid black; ';
 
 const black = document.querySelector('#default');
 const clear = document.querySelector('#clear');
 const erase = document.querySelector('#erase');
 const rgb   = document.querySelector('#rgb');
 
+const color = () => {
+   let n = (Math.random() * 0xfffff * 1000000).toString(16);
+   return '#' + n.slice(0, 6);
+ };
+ 
+console.log(color());
+
 mainDiv.appendChild(div);
 
-for (let i = 0; i<256; i++) {
-   const grid = document.createElement('div');
-   div.appendChild(grid);
-   grid.style.cssText = 'border: 1px solid black; height: 43px; width: 41.5px;'
 
-   black.addEventListener('click', () => {
-      grid.addEventListener('mouseover', () => {
-         grid.style.cssText = 'background-color: black; border: 1px solid black; height: 43px; width: 41.5px;';
+function defaults () {
+
+   for (let i = 0; i<256; i++) {
+      const grid = document.createElement('div');
+      div.appendChild(grid);
+      grid.style.cssText = 'height: 43px; width: 41.5px; border: 1px solid white;'
+
+      black.addEventListener('click', () => {
+         grid.addEventListener('mouseover', () => {
+            grid.style.cssText = 'background-color: black; height: 43px; width: 41.5px; border: 1px solid white;';
+         })
       })
-   })
+
+      clear.addEventListener('click', () => {
+         grid.style.cssText = 'background-color: white; height: 43px; width: 41.5px; border: 1px solid white;';
+         
+      })
+
+      erase.addEventListener('click', () => {
+         grid.addEventListener('mouseover', () => {
+         grid.style.cssText = 'background-color: white; height: 43px; width: 41.5px; border: 1px solid white;'
+      })})
+
+      
+      rgb.addEventListener('click', () => {
+         grid.addEventListener('mouseover', () => {
+            grid.style.cssText = `background-color: ${color()};height: 43px; width: 41.5px; border: 1px solid white;`
+            
+         })
+      })
+}
 }
 
+function newGrid () {
+   for (let i = 0; i<1024; i++) {
+      const grid = document.createElement('div');
+      div.appendChild(grid);
+      grid.style.cssText = 'height: 20px; width: 19.8px; border: 1px solid white;'
+
+      black.addEventListener('click', () => {
+         grid.addEventListener('mouseover', () => {
+            grid.style.cssText = 'background-color: black; height: 20px; width: 19.8px; border: 1px solid white;';
+         })
+      })
+
+      clear.addEventListener('click', () => {
+         grid.style.cssText = 'background-color: white; height: 20px; width: 19.8px; border: 1px solid white;';
+         
+      })
+
+      erase.addEventListener('click', () => {
+         grid.addEventListener('mouseover', () => {
+         grid.style.cssText = 'background-color: white; height: 20px; width: 19.8px; border: 1px solid white;'
+      })})
+
+
+      rgb.addEventListener('click', () => {
+         grid.addEventListener('mouseover', () => {
+            grid.style.cssText = `background-color: ${color()};height: 20px; width: 19.8px; border: 1px solid white;`
+            
+         })
+      })
+}
+}
+
+
+const sixteen = document.querySelector('#sixteen');
+sixteen.addEventListener('click', defaults, {once: true});
+
+const thirty = document.querySelector('#thirty-two');
+thirty.addEventListener('click', newGrid, {once: true});
 
 
